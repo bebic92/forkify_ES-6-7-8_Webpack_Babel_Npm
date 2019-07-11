22e99389aa61cb021c07afd72c19145039e9c3e4
Marin Bebic <=marin.bebic@gmail.com>
2019-07-11 18:07:26 +0200

    Instalirani i konfigurirani webpack i babel, kreirana osnovna struktura projekta kao i osnovni izgled aplikacije(html, css)

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// Global app controller
+
+import str from './models/Search';
+//import {sum, add} from './views/searchView';
+import searchView from './views/searchView';
+
+console.log(`${str}, when you sum numbers 5 and 6 you get ${sum(5,6)}, and when you multiply you get ${add(5,6)} `);
